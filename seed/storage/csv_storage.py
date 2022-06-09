d576e649885175367543980ed1102b5d66208c41
Paul <pkilgarriff@gmail.com>
2022-06-09 15:54:24 +0100

    Update MusicCSVStorage
    - remove add()
    - remove instance variable tracks
    - add error handling to save_library

--- seed/storage/csv_storage.py
+++ seed/storage/csv_storage.py
@@ -3,29 +3,33 @@ from player.music_library import Track
 
 
 class MusicCSVStorage:
-    def __init__(self, path_to_csv):
+    def __init__(self, path_to_csv="tracks.csv"):
         self.path_to_csv = path_to_csv
-        self.tracks = []
 
     def open_library(self):
+        tracks = []
         with open(self.path_to_csv) as csv_file:
             csv_reader = csv.reader(csv_file, delimiter=',')
             for row in csv_reader:
                 trimmed_row = map(lambda x: x.strip(), row)
-                self.tracks.append(
+                tracks.append(
                     Track(*trimmed_row)
                 )
+        return tracks
 
-    def add(self, track_to_add):
-        self.tracks.append(track_to_add)
-
-    def save_library(self):
-        # Responsible for writing self.tracks to a csv
-        with open(self.path_to_csv, mode="w") as csv_file:
-            csv_writer = csv.writer(csv_file, delimiter=',')
-            for track in self.tracks:
-                csv_writer.writerow(
-                    [track.title, track.artist, track.file, track.track_id])
-
-    def all(self):
-        return self.tracks
+    def save_library(self, library_tracks):
+        try:
+            with open(self.path_to_csv, mode="w") as csv_file:
+                csv_writer = csv.writer(csv_file, delimiter=',')
+                for track in library_tracks:
+                    csv_writer.writerow(
+                        [
+                            track.title,
+                            track.artist,
+                            track.file,
+                            track.track_id
+                        ])
+        except:
+            return False
+        else:
+            return True
--- seed/storage/test_csv_storage.py
+++ seed/storage/test_csv_storage.py
@@ -24,7 +24,6 @@ class TestMusicPlayer(unittest.TestCase):
     def test_open_library(self):
         mock_csv = f"{self.DIRECTORY_PATH}test_tracks.csv"
         storage = MusicCSVStorage(mock_csv)
-        storage.open_library()
         expected = [
             Track("Never Come Back", "Caribou", "ncb.mp3",
                   "0QEG3NGmWatNOIAVxudQfd"),
@@ -33,59 +32,14 @@ class TestMusicPlayer(unittest.TestCase):
             Track("Trouble's Coming", "Royal Blood", "troubles_coming.mp3",
                   "6voIJ7OWwRabSZDC77D5Hp"),
         ]
-        self.assertEqual(storage.tracks, expected)
-
-    def test_add(self):
-        mock_csv = f"{self.DIRECTORY_PATH}test_tracks.csv"
-        storage = MusicCSVStorage(mock_csv)
-        added_track = Track("Inspector Norse", "Todd Terje",
-                            "ins_norse.mp3", "2pucDx5Wyz9uHCou4wntHa")
-        storage.open_library()
-        storage.add(added_track)
-        expected = [
-            Track("Never Come Back", "Caribou", "ncb.mp3",
-                  "0QEG3NGmWatNOIAVxudQfd"),
-            Track("Light It Up - Remix", "Major Lazer", "light_it_up.mp3",
-                  "6GKkIuMAiGVDPUkwVESbqC"),
-            Track("Trouble's Coming", "Royal Blood", "troubles_coming.mp3",
-                  "6voIJ7OWwRabSZDC77D5Hp"),
-            Track("Inspector Norse", "Todd Terje",
-                  "ins_norse.mp3", "2pucDx5Wyz9uHCou4wntHa")
-        ]
-        self.assertEqual(storage.tracks, expected)
-
-    def test_add(self):
-        mock_csv = f"{self.DIRECTORY_PATH}test_tracks.csv"
-        storage = MusicCSVStorage(mock_csv)
-        added_track = Track("Inspector Norse", "Todd Terje",
-                            "ins_norse.mp3", "2pucDx5Wyz9uHCou4wntHa")
-        storage.open_library()
-        storage.add(added_track)
-        expected = [
-            Track("Never Come Back", "Caribou", "ncb.mp3",
-                  "0QEG3NGmWatNOIAVxudQfd"),
-            Track("Light It Up - Remix", "Major Lazer", "light_it_up.mp3",
-                  "6GKkIuMAiGVDPUkwVESbqC"),
-            Track("Trouble's Coming", "Royal Blood", "troubles_coming.mp3",
-                  "6voIJ7OWwRabSZDC77D5Hp"),
-            Track("Inspector Norse", "Todd Terje",
-                  "ins_norse.mp3", "2pucDx5Wyz9uHCou4wntHa")
-        ]
-        self.assertEqual(storage.tracks, expected)
+        self.assertEqual(storage.open_library(), expected)
 
     def test_save_library(self):
         mock_csv = f"{self.DIRECTORY_PATH}test_writing_tracks.csv"
+        mock_library = [Track("Trouble's Coming", "Royal Blood", "troubles_coming.mp3",
+                              "6voIJ7OWwRabSZDC77D5Hp")]
         storage = MusicCSVStorage(mock_csv)
-        added_track = Track("Trouble's Coming", "Royal Blood", "troubles_coming.mp3",
-                            "6voIJ7OWwRabSZDC77D5Hp")
-        storage.add(added_track)
-        storage.save_library()
+        storage.save_library(mock_library)
         file = open(mock_csv)
         self.assertEqual(
             str(file.read()), "Trouble's Coming,Royal Blood,troubles_coming.mp3,6voIJ7OWwRabSZDC77D5Hp\n")
-
-
-# file_path agnostic
-# file_type csv
-# import the CSV stuff
-# return an array of items
